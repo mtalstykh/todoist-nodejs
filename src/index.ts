36be6b4cb03abc2363ec6ce95b32d9890b775ee1
mtalstykh <maksimtalstykh@gmail.com>
2019-04-18 19:03:07 +0300

    Add init method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-module.exports = {
-  sayHello() {
-    console.log("Yo world!");
+require('dotenv').config();
+import * as api from './api'
+
+module.exports = class todoist {
+  data: object = null;
+  apiKey: string = null;
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+    this.init();
+  }
+
+  init(): void {
+    return api.sync().then(res => this.data = res.data)
+              .catch(err => console.log(err));
   }
 }
