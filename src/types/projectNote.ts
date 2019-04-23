ce7e24672476cb533e92b806655d5166c0ec8a4c
mtalstykh <maksimtalstykh@gmail.com>
2019-04-23 18:15:24 +0300

    Add interfaces for project note

---
+++ src/types/projectNote.ts
@@ -0,0 +1,47 @@
+import { FileAttachment } from './note';
+
+export interface ProjectNote {
+  "id": number;
+  // The id of the user that posted the note
+  "posted_uid": number;
+  // The item which the note is part of
+  "projecr_id": number;
+  // The content of the note
+  "content": string;
+  // A file attached to the note
+  "file_attachment": FileAttachment;
+  // A list of user ids to notify
+  "uids_to_notify": {
+    [index: number]: number
+  };
+  // Whether the note is marked as deleted (where 1 is true and 0 is false)
+  "is_deleted": number;
+  // Whether the note is marked as archived (where 1 is true and 0 is false)
+  "is_archived": number;
+  // The date when the note was posted
+  "posted": string;
+  // TODO Rep;ace reactions type
+  // List of emoji reactions and corresponding user ids
+  "reactions": object;
+}
+
+export interface AddOptions {
+  // The project which the note is part of
+  "project_id": number;
+  // The content of the note
+  "content": string;
+  // A file attached to the note
+  "file_attachment"?: FileAttachment;
+}
+
+export interface UpdateOptions {
+  "id": number;
+  // The content of the note
+  "content": string;
+  // A file attached to the note
+  "file_attachment"?: FileAttachment;
+}
+
+export interface DeleteOptions {
+  "id": number;
+}
