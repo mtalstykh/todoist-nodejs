32d12da5286d209b1a428d8fb0b7b7886aae0911
mtalstykh <maksimtalstykh@gmail.com>
2019-04-26 16:55:44 +0300

    Add update method

--- src/services/state.ts
+++ src/services/state.ts
@@ -1,3 +1,17 @@
+import { State } from '../types/state';
+import api  from './request';
+import { AxiosResponse, AxiosError } from 'axios';
+
 export class StateService {
-  // void
+  state: State = null;
+
+  // TODO Replace any
+  update(): any {
+    api.sync()
+       .then((responce: AxiosResponse) => {
+         this.state = responce.data;
+         console.log(this.state);
+       })
+       .catch((err: AxiosError) => console.log(err));
+  }
 }
