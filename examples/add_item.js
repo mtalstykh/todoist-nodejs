4c9a903c4eb830b01d4b6d846ffb0cd5352ff313
mtalstykh <maksimtalstykh@gmail.com>
2019-05-10 12:52:13 +0300

    Prettiered

--- examples/add_item.js
+++ examples/add_item.js
@@ -1,8 +1,14 @@
-const todoist = require('../build/index');
+const todoist = require("../build/index");
 
 const api = new todoist(process.env.TOKEN_API);
-api.sync()
-   .then(() => api.items.add('task1', {priority: 4, due: { string: 'today' }}))
-   .then(
-      () => api.commit()
-    );
+api
+  .sync()
+  .then(() =>
+    api.items.add('task1', {
+      priority: 4,
+      due: {
+        string: 'today'
+      }
+    })
+  )
+  .then(() => api.commit());
